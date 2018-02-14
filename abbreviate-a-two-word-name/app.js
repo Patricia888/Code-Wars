fb5f629eeb239539e6b412e4753f1f53d447b8b7
Patricia G Raftery <praftery8@gmail.com>
2018-02-14 07:04:50 -0800

    abbreviate CW done

--- abbreviate-a-two-word-name/app.js
+++ abbreviate-a-two-word-name/app.js
@@ -1,2 +1,11 @@
 'use strict';
 
+function abbrevName(name){
+  let splitNameArray = name.split(" ");
+
+  let firtName = splitNameArray[0][0];
+  let lastName = splitNameArray[1][0];
+
+  return firtName.toUpperCase() + '.' + lastName.toUpperCase();
+
+}
