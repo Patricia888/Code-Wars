38c7f16c30aa6c3e43074681c4636bd880c78227
Patricia G Raftery <praftery8@gmail.com>
2018-02-16 13:38:58 -0800

    finished kata

--- insert-dashes/app.js
+++ insert-dashes/app.js
@@ -1,2 +1,17 @@
 'use strict';
 
+function insertDash(num) {
+  let numbers = '' + num;
+  let result = '';
+  
+  let oddArray = ['1', '3', '5', '7', '9'];
+  
+  for(var i = 0; i < numbers.length; i++) {
+    if(i > 0 && oddArray.includes(numbers[i-1]) && oddArray.includes(numbers[i])) {
+      result += '-';
+    }
+    result += numbers[i];
+  }
+
+  return result;
+}
